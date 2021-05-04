df9184a501ce50c7d606082569eab618cac15f9f
Unknown <niasio@free.fr>
2021-05-04 08:36:49 +0200

    SP-005: main.py comments

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
+"""
+SnownyPython - Python maze game with Turtle module
+Author: Jimmy F.
+Date: 23/03/2021
+Version: 0.1
+
+This class allows to execute the game
+"""
+
 from Character import *
 
-listen()
-onkeypress(deplacer_gauche, "Left")
+listen() #Allows you to listen to what the player is going to type on his keyboard
+
+#Definition of keys with movement functions
+onkeypress(deplacer_gauche, "Left") 
 onkeypress(deplacer_droite, "Right")
 onkeypress(deplacer_haut, "Up")
 onkeypress(deplacer_bas, "Down")
-mainloop()
+
+mainloop() #Tells the window to wait for the user to do something,
+           #although in this case there’s not much for the user to do except close the window
